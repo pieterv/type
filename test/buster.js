3b81f81d2090f4f0f13e79bff75962c672ade5da
Pieter Vanderwerff <pieter@heyday.co.nz>
2012-06-10 21:10:08 +1200

    FEATURE: Basic unit tests with buster

---
+++ test/buster.js
@@ -0,0 +1,8 @@
+var config = module.exports;
+
+config[ 'Node test' ] = {
+	env: 'node',
+	tests: [
+		'*-test.js'
+	]
+};
---
+++ test/type-test.js
@@ -0,0 +1,85 @@
+var type = require( '../src/type' ),
+
+	// Buster setup
+	buster = require( 'buster' ),
+	expect = buster.assertions.expect;
+
+buster.spec.expose();
+
+describe( 'Type', function() {
+
+	it( 'basic state', function() {
+		expect( type ).toBeDefined();
+		expect( type ).toBeFunction();
+		expect( type() ).toBeFunction();
+		expect( new ( type() )() ).toBeObject();
+	} );
+
+	it( 'can create types', function() {
+		var Foo = type( {
+				foo: 'bar',
+				bar: function() {}
+			} ),
+			foo1 = new Foo();
+
+		expect( foo1.foo ).toBe( 'bar' );
+		expect( foo1.bar ).toBeFunction();
+	} );
+
+	it( 'can call initialize function on creation', function() {
+		var init = this.spy(),
+			Foo = type( {
+				initialize: init,
+				harry: function() {}
+			} );
+
+		new Foo( 'test', 'test2' );
+
+		expect( init ).toHaveBeenCalledOnce();
+		expect( init ).toHaveBeenCalledWith( 'test', 'test2' );
+
+		new Foo( 'test3' );
+
+		expect( init ).toHaveBeenCalledTwice();
+		expect( init ).toHaveBeenCalledWith( 'test3' );
+
+	} );
+
+	it( 'types are not references', function() {
+		var Foo = type( {
+				bar: 3
+			} ), foo1, foo2;
+
+		foo1 = new Foo();
+		foo2 = new Foo();
+
+		expect( foo1.bar ).toBe( 3 );
+		expect( foo2.bar ).toBe( 3 );
+
+		foo1.bar = 1;
+
+		expect( foo1.bar ).toBe( 1 );
+		expect( foo2.bar ).toBe( 3 );
+
+	} );
+
+	it( 'can extend types', function() {
+		var Foo = type( {
+				bar: 3
+			} ), Bar, foo1, bar1;
+
+		Bar = Foo.extend( {
+			bar: 10,
+			bob: {}
+		} );
+
+		foo1 = new Foo();
+		bar1 = new Bar();
+
+		expect( foo1.bar ).toBe( 3 );
+		expect( foo1.bob ).not.toBeDefined();
+
+		expect( bar1.bar ).toBe( 10 );
+		expect( bar1.bob ).toBeObject();
+	} );
+} );
